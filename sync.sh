2f69518544970ef7aff1f3fdb6757463e53d40f5
李聪 <licong@360.net>
2019-04-12 16:36:57 +0800

    解决 docker 没有登录的问题

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker login -u hellojukay -p "${p-dockerhub}"
 docker pull k8s.gcr.io/kube-apiserver:v1.13.0
 docker tag k8s.gcr.io/kube-apiserver:v1.13.0 hellojukay/kube-apiserver:v1.13.0
 docker push hellojukay/kube-apiserver:v1.13.0
