bbf11015dc739364e9cd41e13efd5eff96fdec76
李聪 <licong@360.net>
2019-04-12 17:41:57 +0800

    update script

--- sync.groovy
+++ sync.groovy
@@ -1,13 +1,21 @@
 #!/usr/bin/env groovy
 
+def sh(String cmd) {
+    println("cmd: $cmd")
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    def proc = cmd.execute()
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitForOrKill(1000)
+    println "out:$sout"
+    println "err: $serr"
+}
 def lines = new File("list.txt").readLines()
 for(image in lines) {
     if(image.length() <=1) {
         continue
     }
     def tag = image.replace("k8s.gcr.io","hellojukay")
-    println(tag)
-    "docker pull ${image} ".execute()
-    "docker tag ${image} ${tag}".execute()
-    "docker push ${image} ".execute()
+    sh("docker pull ${image}")
+    sh("docker tag ${image} ${tag}")
+    sh("docker push ${tag}")
 }
