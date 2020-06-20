6c672aa1ff04e042e03acd55bde97c0b82be5227
hellojukay <licong@qianxin.com>
2020-06-20 22:40:10 +0800

    add v1.18.4_list.txt

--- pull.pl
+++ pull.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl
-open(HF,"<","v1.18.3_list.txt");
+open(HF,"<","v1.18.4_list.txt");
 while(<HF>) {
 	chomp $_;
+    my $image = $_;
 	if($_ =~ m/.*\/(.*)/){
 		my $cmd = "docker pull hellojukay/$1";
 		printf "%s\n",$cmd;
 		system($cmd);
+        system("docker tag hellojukay$1 $image");
 	}
 }
