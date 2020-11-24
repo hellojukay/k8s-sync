9679fbed53d1eb30cca65f855fe8d096ca52011f
licong <licong@qianxin.com>
2020-11-24 01:57:57 +0000

    fix format

--- pull.pl
+++ pull.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 open(HF,"<",$ARGV[0]);
 while(<HF>) {
-	chomp $_;
+    chomp $_;
     my $image = $_;
-	if($_ =~ m/.*\/(.*)/){
-		my $cmd = "docker pull hellojukay/$1";
-		printf "%s\n",$cmd;
-		system($cmd);
-       	system("docker tag hellojukay/$1 $image");
+    if($_ =~ m/.*\/(.*)/){
+        my $cmd = "docker pull hellojukay/$1";
+        printf "%s\n",$cmd;
+        system($cmd);
+        system("docker tag hellojukay/$1 $image");
         system("docker rmi hellojukay/$1");
-	}
+    }
 }
